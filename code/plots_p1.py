e953a875ee6a786b163d3335cd6c959e9cda668b
StevenGolovkine <steven_golovkine@icloud.com>
2023-03-30 15:14:19 +0100

    Simulation + plots

--- code/plots_p1.py
+++ code/plots_p1.py
@@ -83,22 +83,22 @@ def load_results(path):
     # Select data time
     data_time = data.loc[:, ['P', 'N', 'M', 'ratiotime']].\
         sort_values(['P', 'N', 'M']).\
-        replace({'P': {2: '2', 10: '10', 20: '20', 50: '50'}})
+        replace({'M': {25: '25', 50: '50', 75: '75', 100: '100'}})
 
     # Select data MISE
     data_mise = data.loc[:, ['P', 'N', 'M', 'ratiomise']].\
         sort_values(['P', 'N', 'M']).\
-        replace({'P': {2: '2', 10: '10', 20: '20', 50: '50'}})
+        replace({'M': {25: '25', 50: '50', 75: '75', 100: '100'}})
 
     # Select data logAE
     data_logAE = data_logAE.loc[:, ['P', 'N', 'M', 'K', 'ratiologAE']].\
         sort_values(['P', 'N', 'M', 'K']).\
-        replace({'P': {2: '2', 10: '10', 20: '20', 50: '50'}})
+        replace({'M': {25: '25', 50: '50', 75: '75', 100: '100'}})
 
     # Select data logAE
     data_ise = data_ise.loc[:, ['P', 'N', 'M', 'K', 'ratioise']].\
         sort_values(['P', 'N', 'M', 'K']).\
-        replace({'P': {2: '2', 10: '10', 20: '20', 50: '50'}})
+        replace({'M': {25: '25', 50: '50', 75: '75', 100: '100'}})
 
     return data_time, data_mise, data_logAE, data_ise
 
@@ -107,13 +107,13 @@ def plot_mise(results):
     """Plot the results of the simulation in term of MISE."""
     gg = sns.catplot(
         data=results,
-        x="ratiomise", y="P",
+        x="ratiomise", y="M",
         kind="box",
         height=7,
         aspect=1
     )
     gg.set(xlabel="MISE($\mathcal{X}$, $\widehat{\mathcal{X}}$) / MISE($\mathcal{X}$, $\widetilde{\mathcal{X}}$)", ylabel="")
-    gg.set_yticklabels(["$P = 2$", "$P = 10$", "$P = 20$", "$P = 50$"], size=20)
+    gg.set_yticklabels(["$M = 25$", "$M = 50$", "$M = 75$", "$M = 100$"], size=10)
     for ax in gg.axes.flat:
         ax.axvline(x=1, color='r', lw=1, ls='--')
     gg.fig.tight_layout()
@@ -124,13 +124,13 @@ def plot_computation_time(results):
     """Plot the results of the simulation in term of computation time."""
     gg = sns.catplot(
         data=results,
-        x="ratiotime", y="P",
+        x="ratiotime", y="M",
         kind="violin",
         height=7,
         aspect=1
     )
     gg.set(xlabel="Ratio of computation time", ylabel="")
-    gg.set_yticklabels(["$P = 2$", "$P = 10$", "$P = 20$", "$P = 50$"], size=20)
+    gg.set_yticklabels(["$M = 25$", "$M = 50$", "$M = 75$", "$M = 100$"], size=10)
     for ax in gg.axes.flat:
         ax.axvline(x=1, color='r', lw=1, ls='--')
         ax.set_xscale("log")
@@ -142,14 +142,14 @@ def plot_ise(results):
     """Plot the results of the simulation in term of ISE."""
     gg = sns.catplot(
         data=results,
-        x="ratioise", y="P", hue='K',
+        x="ratioise", y="M", hue='K',
         kind="box",
         height=7,
         aspect=1
     )
     gg.set(xlabel="ISE($\phi_k$, $\widehat{\phi}_k$) / ISE($\phi_k$, $\widetilde{\phi}_k$)", ylabel="")
-    gg.set_yticklabels(["$P = 2$", "$P = 10$", "$P = 20$", "$P = 50$"], size=20)
-    gg.set(xlim=(0, 2))
+    gg.set_yticklabels(["$M = 25$", "$M = 50$", "$M = 75$", "$M = 100$"], size=10)
+    # gg.set(xlim=(0, 2))
     for ax in gg.axes.flat:
         ax.axvline(x=1, color='r', lw=1, ls='--')
     gg.fig.tight_layout()
@@ -167,14 +167,14 @@ def plot_logAE(results):
     """Plot the results of the simulation in term of logAE."""
     gg = sns.catplot(
         data=results,
-        x="ratiologAE", y="P", hue='K',
+        x="ratiologAE", y="M", hue='K',
         kind="box",
         height=7,
         aspect=1
     )
     gg.set(xlabel="$\log-$AE($\lambda_k$, $\widehat{\lambda}_k$) / $\log-$AE($\lambda_k$, $\widetilde{\lambda}_k$)", ylabel="")
-    gg.set_yticklabels(["$P = 2$", "$P = 10$", "$P = 20$", "$P = 50$"], size=20)
-    gg.set(xlim=(0, 2))
+    gg.set_yticklabels(["$M = 25$", "$M = 50$", "$M = 75$", "$M = 100$"], size=10)
+    # gg.set(xlim=(0, 2))
     for ax in gg.axes.flat:
         ax.axvline(x=1, color='r', lw=1, ls='--')
     gg.fig.tight_layout()
@@ -206,46 +206,45 @@ if __name__ == "__main__":
 
     # Plot computation time
     for N in ['25', '50', '75', '100']:
-        for M in ['25', '50', '75', '100']:
-            temp = results_time.query(f'N == {N} and M == {M}')
-            plot_computation_time(temp)
-            plt.savefig(
-                f'{args.out_folder}/{path_split[1]}/computation_time/'
-                f'computation_time_N{N}_M{M}.{args.format}',
-                format=args.format,
-                bbox_inches='tight'
-            )
-            plt.close()
+        temp = results_time.query(f'N == {N}')
+        plot_computation_time(temp)
+        plt.savefig(
+            f'{args.out_folder}/{path_split[1]}/computation_time/'
+            f'computation_time_N{N}.{args.format}',
+            format=args.format,
+            bbox_inches='tight'
+        )
+        plt.close()
 
-            # Plot MISE
-            temp = results_mise.query(f'N == {N} and M == {M}')
-            plot_mise(temp)
-            plt.savefig(
-                f'{args.out_folder}/{path_split[1]}/mise/'
-                f'mise_N{N}_M{M}.{args.format}',
-                format=args.format,
-                bbox_inches='tight'
-            )
-            plt.close()
+        # Plot MISE
+        temp = results_mise.query(f'N == {N}')
+        plot_mise(temp)
+        plt.savefig(
+            f'{args.out_folder}/{path_split[1]}/mise/'
+            f'mise_N{N}.{args.format}',
+            format=args.format,
+            bbox_inches='tight'
+        )
+        plt.close()
 
-            # Plot log-AE
-            temp = results_logAE.query(f'N == {N} and M == {M}')
-            plot_logAE(temp)
-            plt.savefig(
-                f'{args.out_folder}/{path_split[1]}/logAE/'
-                f'logAE_N{N}_M{M}.{args.format}',
-                format=args.format,
-                bbox_inches='tight'
-            )
-            plt.close()
+        # Plot log-AE
+        temp = results_logAE.query(f'N == {N}')
+        plot_logAE(temp)
+        plt.savefig(
+            f'{args.out_folder}/{path_split[1]}/logAE/'
+            f'logAE_N{N}.{args.format}',
+            format=args.format,
+            bbox_inches='tight'
+        )
+        plt.close()
 
-            # Plot ISE
-            temp = results_ise.query(f'N == {N} and M == {M}')
-            plot_ise(temp)
-            plt.savefig(
-                f'{args.out_folder}/{path_split[1]}/ise/'
-                f'ise_N{N}_M{M}.{args.format}',
-                format=args.format,
-                bbox_inches='tight'
-            )
-            plt.close()
+        # Plot ISE
+        temp = results_ise.query(f'N == {N}')
+        plot_ise(temp)
+        plt.savefig(
+            f'{args.out_folder}/{path_split[1]}/ise/'
+            f'ise_N{N}.{args.format}',
+            format=args.format,
+            bbox_inches='tight'
+        )
+        plt.close()
