89b0e00c51ecf95b5d77c4bfd684dd9744f7a8ca
StevenGolovkine <steven_golovkine@icloud.com>
2024-05-04 20:39:09 +0100

    Simulation

--- code/functions.py
+++ code/functions.py
@@ -1,53 +1,40 @@
 ################################################################################
 # Utility functions for the simulations of univariate functional data
-# S. Golovkine - 17/01/2023
+# S. Golovkine - 05/04/2024
 ################################################################################
 
 # Load packages
 import numpy as np
 
-from FDApy.preprocessing.dim_reduction.fpca import UFPCA, MFPCA
-from FDApy.preprocessing.dim_reduction.fcp_tpa import FCPTPA
 from FDApy.representation.basis import Basis, MultivariateBasis
+from FDApy.representation.argvals import DenseArgvals
+from FDApy.representation.values import DenseValues
 from FDApy.representation.functional_data import (
     DenseFunctionalData,
+    BasisFunctionalData,
     MultivariateFunctionalData
 )
-from FDApy.simulation.karhunen import KarhunenLoeve
+from FDApy.simulation.karhunen import (
+    _initialize_centers,
+    _initialize_clusters_std,
+    _make_coef
+)
+from FDApy.simulation.simulation import (
+    _add_noise_univariate_data
+)
 
 # Variables
 
 # Functions
-def make_diff_op(degree, dim):
-    if degree == 0:
-        return np.eye(dim)
-    else:
-        return np.diff(make_diff_op(degree - 1, dim), axis=0)
-
-
-def simulate_data(
-    n_obs,
-    n_points,
-    basis_name,
-    n_functions,
-    dimension,
-    seed
-):
-    """Simulation function for univariate functional data.
+def simulate_data(n_obs, noise_variance, seed):
+    """Simulation function.
 
     Parameters
     ----------
     n_obs: int
         Number of observations to simulated.
-    n_points: int
-        Number of points used to simulate the data. For 2D-data, the number
-        of generated points is n_points * n_points.
-    basis_name: str
-        Name of the basis to used.
-    n_functions: int
-        Number of basis functions to used.
-    dimension: str
-        Dimension of the data (1D or 2D).
+    noise_variance: float
+        Variance of the noise.
     seed: int
         A seed for reproducibility.
 
@@ -57,254 +44,72 @@ def simulate_data(
         The simulated dataset.
 
     """
-    # Define the basis
-    argvals = np.linspace(0, 1, n_points)
-    basis = Basis(
-        name=basis_name,
-        n_functions=n_functions,
+    # Set the RNG
+    rng = np.random.default_rng(seed)
+
+    # Basis definition
+    argvals = DenseArgvals({
+        'input_dim_0': np.linspace(0, 1, 101),
+        'input_dim_1': np.linspace(0, 0.5, 51),
+    })
+    basis_1 = Basis(
+        name=['fourier', 'fourier'],
+        n_functions=[5, 5],
+        argvals=argvals
+    )
+    
+    argvals = DenseArgvals({
+        'input_dim_0': np.linspace(-1, 1, 201)
+    })
+    basis_2 = Basis(
+        name='legendre',
+        n_functions=25,
         argvals=argvals,
-        dimension=dimension,
-        add_intercept=True
     )
 
-    # Generate data
-    kl = KarhunenLoeve(basis_name=None, basis=basis, random_state=seed)
-    kl.new(n_obs=n_obs, clusters_std='exponential')
-    return kl
+    # Normalization of the basis
+    u1 = rng.uniform(0.2, 0.8, 1)
+    u2 = rng.uniform(0.2, 0.8, 1)
+    alpha = u1 / (u1 + u2)
 
-
-def simulate_data_multivariate(
-    n_components,
-    n_obs,
-    n_points,
-    basis_name,
-    n_functions,
-    seed
-):
-    """Simulation function for univariate functional data.
-
-    Parameters
-    ----------
-    n_components: int
-        Number of components to simulate.
-    n_obs: int
-        Number of observations to simulate.
-    n_points: int
-        Number of points used to simulate the data.
-    basis_name: str
-        Name of the basis to used.
-    n_functions: int
-        Number of basis functions to used.
-    seed: int
-        A seed for reproducibility.
-
-    Returns
-    -------
-    KarhunenLoeve
-        The simulated dataset.
-
-    """
-    # Define the argvals
-    rng = np.random.RandomState(seed)
-    start_points = rng.uniform(-1, 1, n_components)
-    end_points = rng.uniform(-1, 1, n_components)
-    argvals = [
-        np.sort(np.linspace(begin, end, n_points))
-        for begin, end in zip(start_points, end_points)
-    ]
-
-    # Define the basis
+    # Define multivariate basis
     basis = MultivariateBasis(
-        simulation_type='split',
-        n_components=n_components,
-        name=basis_name,
-        n_functions=n_functions,
-        argvals=argvals,
-        dimension=n_components * ['1D']
-    )
+        name='given',
+        argvals=[basis_1.argvals, basis_2.argvals],
+        values=[
+            np.sqrt(alpha) * basis_1.values,
+            np.sqrt(1 - alpha) * basis_2.values
+        ]
+    )   
+
+    # Initialize parameters
+    n_features = basis.n_obs
+    centers = _initialize_centers(n_features, 1, None)
+    clusters_std = _initialize_clusters_std(n_features, 1, 'exponential')
+
+    # Generate coefficients
+    rnorm = rng.multivariate_normal
+    coef, _ = _make_coef(n_obs, n_features, centers, clusters_std, rnorm)
 
     # Generate data
-    kl = KarhunenLoeve(basis_name=None, basis=basis, random_state=seed)
-    kl.new(n_obs=n_obs, clusters_std='exponential')
-    return kl
+    data = MultivariateFunctionalData([
+        BasisFunctionalData(basis=basis_univariate, coefficients=coef).to_grid()
+        for basis_univariate in basis.data
+    ])
 
+    # Generate noise day
+    data_noisy = MultivariateFunctionalData([
+        _add_noise_univariate_data(
+            data_univariate, noise_variance=noise_variance, rnorm=rng.normal
+        ) for data_univariate in data.data
+    ])
 
-def ufpca_covariance(data, n_components):
-    """Perform an estimation of the eigencomponents using the diagonalization
-    of the covariance operator.
-
-    Parameters
-    ----------
-    data: DenseFunctionalData
-        A dataset
-    n_components: Union[int, float, None]
-        Number of components to keep. If `n_components` is `None`, all
-        components are kept, ``n_components == min(n_samples, n_features)``.
-        If `n_components` is an integer, `n_components` are kept. If
-        `0 < n_components < 1`, select the number of components such that the
-        amount of variance that needs to be explained is greater than the
-        percentage specified by `n_components`.
-
-    Returns
-    -------
-    Union[UFPCA, FCPTPA]
-        An object representing the diagonalization of the covariance operator.
-
-    """
-    if data.n_dim == 1:
-        ufpca = UFPCA(
-            n_components=n_components, method='covariance', normalize=False
-        )
-        ufpca.fit(data)
-    elif data.n_dim == 2:
-        n_points = data.n_points
-        mat_v = make_diff_op(2, n_points['input_dim_0'])
-        mat_w = make_diff_op(2, n_points['input_dim_1'])
-
-        ufpca = FCPTPA(n_components=n_components, normalize=True)
-        ufpca.fit(
-            data,
-            penalty_matrices={
-                'v': np.dot(mat_v.T, mat_v),
-                'w': np.dot(mat_w.T, mat_w)
-            },
-            alpha_range={
-                'v': (1e-4, 1e4),
-                'w': (1e-4, 1e4)
-            },
-            tolerance=1e-4, max_iteration=15, adapt_tolerance=True
-        )
-    return ufpca
-
-
-def mfpca_covariance(data, n_components):
-    """Perform an estimation of the eigencomponents using the diagonalization
-    of the covariance operator.
-
-    Parameters
-    ----------
-    data: MultivariateFunctonalData
-        A dataset
-    n_components: Union[int, float, None]
-        Number of components to keep. If `n_components` is `None`, all
-        components are kept, ``n_components == min(n_samples, n_features)``.
-        If `n_components` is an integer, `n_components` are kept. If
-        `0 < n_components < 1`, select the number of components such that the
-        amount of variance that needs to be explained is greater than the
-        percentage specified by `n_components`.
-
-    Returns
-    -------
-    MFPCA
-        An object representing the diagonalization of the covariance operator.
-
-    """
-    n_components = data.n_functional * [n_components]
-    mfpca = MFPCA(
-        n_components=n_components, method='covariance', normalize=True
-    )
-    mfpca.fit(data)
-    return mfpca
-
-
-def ufpca_inner_product(data, n_components):
-    """Perform an estimation of the eigencomponents using the diagonalization
-    of the inner product matrix.
-
-    Parameters
-    ----------
-    data: DenseFunctionalData
-        A dataset
-    n_components: Union[int, float, None]
-        Number of components to keep. If `n_components` is `None`, all
-        components are kept, ``n_components == min(n_samples, n_features)``.
-        If `n_components` is an integer, `n_components` are kept. If
-        `0 < n_components < 1`, select the number of components such that the
-        amount of variance that needs to be explained is greater than the
-        percentage specified by `n_components`.
-
-    Returns
-    -------
-    UFPCA
-        An object representing the diagonalization of the inner-product matrix.
-
-    """
-    ufpca = UFPCA(n_components=n_components, method='inner-product')
-    ufpca.fit(data)
-    return ufpca
-
-
-def mfpca_inner_product(data, n_components):
-    """Perform an estimation of the eigencomponents using the diagonalization
-    of the inner product matrix.
-
-    Parameters
-    ----------
-    data: MultivariateFunctionalData
-        A dataset
-    n_components: Union[int, float, None]
-        Number of components to keep. If `n_components` is `None`, all
-        components are kept, ``n_components == min(n_samples, n_features)``.
-        If `n_components` is an integer, `n_components` are kept. If
-        `0 < n_components < 1`, select the number of components such that the
-        amount of variance that needs to be explained is greater than the
-        percentage specified by `n_components`.
-
-    Returns
-    -------
-    MFPCA
-        An object representing the diagonalization of the inner-product matrix.
-
-    """
-    mfpca = MFPCA(n_components=n_components, method='inner-product')
-    mfpca.fit(data)
-    return mfpca
-
-
-def compute_scores(data, fpca):
-    """Compute the scores using numerical integration.
-
-    Parameters
-    ----------
-    data: Union[DenseFunctionalData, MultivariateFunctionalData]
-        A dataset.
-    fpca: Union[UFPCA, FCPTPA, MFPCA]
-        An UFPCA or FCPTPA or MFPCA object.
-
-    Returns
-    -------
-    npt.NDArray[np.float64]
-        An estimation of the projection of the scores using numerical
-        integration.
-
-    """
-    if isinstance(fpca, FCPTPA):
-        return fpca.transform(data)
-    else:
-        if fpca.method == 'inner-product':
-            return fpca.transform(data, method='InnPro')
-        else:
-            return fpca.transform(data, method='NumInt')
-
-
-def compute_reconstruction(fpca, scores):
-    """Compute the reconstruction of the curves.
-
-    Parameters
-    ----------
-    ufpca: Union[UFPCA, FCPTPA, MFPCA]
-        An UFPCA or FCPTPAobject.
-    scores: npt.NDArray[np.float64]
-        The scores
-
-    Returns
-    -------
-    Union[DenseFunctionalData, MultivariateFunctionalData]
-        The reconstructed dataset.
-
-    """
-    return fpca.inverse_transform(scores)
-
+    return {
+        'data': data,
+        'data_noisy': data_noisy,
+        'basis': basis,
+        'eigenvalues': clusters_std[:, 0]
+    }
 
 def flip(
     data: DenseFunctionalData,
@@ -374,6 +179,7 @@ def ISE(data_true, data_recons, n_estim=None):
                 ise[p] = np.trapz(y=diff_squared, x=data_t[p].argvals['input_dim_0'])
         res[idx] = np.sum(ise)
     return res
+
 
 def MISE(data_true, data_recons, n_estim=None):
     """Compute MISE between two multivariate functional datasets.
--- code/main.py
+++ code/main.py
@@ -1,23 +1,20 @@
 ################################################################################
-# Scripts to run the simulation for the paper "A geometric interpretation of 
-# the multivariate functional principal components analysis"
-# S. Golovkine - 17/01/2023
+# Scripts to run the simulation for the paper "On the use of the Gram matrix 
+# for multivariate functional principal analysis"
+# S. Golovkine - 05/04/2024
 ################################################################################
 
 # Load packages
 import argparse
-import json
+import pickle
 import multiprocessing
 import os
-import re
 import time
-
-import matplotlib.pyplot as plt
 
 from joblib import Parallel, delayed
 
 from FDApy.representation.functional_data import MultivariateFunctionalData
-from FDApy.visualization.plot import plot_multivariate
+from FDApy.preprocessing import MFPCA
 from functions import *
 
 # Argument parser for the file
@@ -35,12 +32,8 @@ parser.add_argument(
     help="Number of observations"
 )
 parser.add_argument(
-    "-m", "--number_points", default=50,
-    help="Number of points per curves"
-)
-parser.add_argument(
-    "-p", "--number_components", default=1,
-    help="Number of components to simulate"
+    "-noise", "--noise_variance", default=0.25,
+    help="Variance of the noise"
 )
 parser.add_argument(
     "-k", "--number_components_estimate", default=1,
@@ -52,118 +45,175 @@ args = parser.parse_args()
 NUM_CORES = multiprocessing.cpu_count()
 N_SIMU = int(args.number_simulation)
 N = int(args.number_observation)
-M = int(args.number_points)
-P = int(args.number_components)
+noise_variance = float(args.noise_variance)
 n_components = int(args.number_components_estimate)
 
 # Functions
-def run_simulation(idx, n_obs, n_points, n_components):
+def run_simulation(idx, n_obs, noise_variance, n_components, PATH):
     """Run a simulation"""
     print(f'Simulation {idx}')
 
-    if P == 1:  # Simulate univariate functional data
-        basis_name = 'fourier'
-        dimension = '2D'
-        n_functions = 10
+    simulation = simulate_data(
+        n_obs=n_obs,
+        noise_variance=noise_variance,
+        seed=idx
+    )
 
-        kl = simulate_data(
-            n_obs=n_obs,
-            n_points=n_points,
-            basis_name=basis_name,
-            n_functions=n_functions,
-            dimension=dimension,
-            seed=idx
-        )
-    else:  # Simulate multivariate functional data
-        basis_name = 'fourier'
-        n_functions = 10
-        kl = simulate_data_multivariate(
-            n_components=P,
-            n_obs=n_obs,
-            n_points=n_points,
-            basis_name=basis_name,
-            n_functions=n_functions,
-            seed=idx
-        )
-    data = kl.data
-    eigenfunctions = kl.basis  # MultivariateFunctionalData(kl.basis)
-    eigenvalues = kl.eigenvalues
+    data = simulation['data']
+    data_noisy = simulation['data_noisy']
+    eigenfunctions = simulation['basis']
+    eigenvalues = simulation['eigenvalues']
 
-    # Perform FPCA covariance
-    if P == 1:
-        start = time.time()
-        fpca_cov = ufpca_covariance(data, n_components)
-        time_cov = time.time() - start
+    ##########################################################################
+    print(f"Run MFPCA for data without noise (simulation {idx})")
 
-        # Perform FPCA inner-product
-        start = time.time()
-        fpca_inn = ufpca_inner_product(data, n_components)
-        time_inn = time.time() - start
-    else:
-        start_cov = time.time()
-        fpca_cov = mfpca_covariance(data, n_components)
-        time_cov = time.time() - start_cov
+    # Run MFPCA with FCPTPA and UFPCA
+    univariate_expansions = [
+        {'method': 'FCPTPA', 'n_components': 20},
+        {'method': 'UFPCA', 'n_components': 15}
+    ]
+    mfpca_covariance = MFPCA(
+        n_components=n_components,
+        method='covariance',
+        univariate_expansions=univariate_expansions
+    )
+    start = time.process_time()
+    mfpca_covariance.fit(data)
+    time_covariance = time.process_time() - start
 
-        # Perform FPCA inner-product
-        start_inn = time.time()
-        fpca_inn = mfpca_inner_product(data, n_components)
-        time_inn = time.time() - start_inn
 
-    # Compute scores
-    scores_cov = compute_scores(data, fpca_cov)
-    scores_inn = compute_scores(data, fpca_inn)
+    # Run MFPCA with PSplines
+    univariate_expansions = [
+        {'method': 'PSplines', 'penalty': [0, 0]},
+        {'method': 'PSplines', 'penalty': [0]}
+    ]
+    mfpca_psplines = MFPCA(
+        n_components=n_components,
+        method='covariance',
+        univariate_expansions=univariate_expansions
+    )
+    start = time.process_time()
+    mfpca_psplines.fit(data)
+    time_psplines = time.process_time() - start
 
-    # Compute reconstruction
-    data_f_cov = compute_reconstruction(fpca_cov, scores_cov)
-    data_f_inn = compute_reconstruction(fpca_inn, scores_inn)
 
-    # Get eigenvalues
-    eigenvalues_cov = fpca_cov.eigenvalues
-    eigenvalues_inn = fpca_inn.eigenvalues
+    # Run MFPCA with Gram matrix
+    mfpca_gram = MFPCA(n_components=n_components, method='inner-product')
+    start = time.process_time()
+    mfpca_gram.fit(data)
+    time_gram = time.process_time() - start
 
-    # Get eigenfunctions
-    eigenfunctions_cov = fpca_cov.eigenfunctions
-    eigenfunctions_inn = fpca_inn.eigenfunctions
 
-    # Compute errors
-    mise_cov = MISE(data, data_f_cov)
-    mise_inn = MISE(data, data_f_inn)
+    # Estimate scores
+    scores_covariance = mfpca_covariance.transform(data)
+    scores_psplines = mfpca_psplines.transform(data)
+    scores_gram = mfpca_gram.transform(method='InnPro')
 
-    ise_cov = ISE(eigenfunctions, eigenfunctions_cov, n_estim=n_components)
-    ise_inn = ISE(eigenfunctions, eigenfunctions_inn, n_estim=n_components)
-    
-    ae_cov = logAE(eigenvalues, eigenvalues_cov, n_components)
-    ae_inn = logAE(eigenvalues, eigenvalues_inn, n_components)
-    
 
-    return {
-        'time_cov': time_cov,
-        'time_inn': time_inn,
-        'mise_cov': mise_cov,
-        'mise_inn': mise_inn,
-        'logAE_cov': list(ae_cov),
-        'logAE_inn': list(ae_inn),
-        'ise_cov': list(ise_cov),
-        'ise_inn': list(ise_inn),
-        'n_comp_cov': fpca_cov.eigenfunctions.n_obs,
-        'n_comp_inn': fpca_inn.eigenfunctions.n_obs
+    # Reconstructing the data
+    data_recons_covariance = mfpca_covariance.inverse_transform(
+        scores_covariance
+    )
+    data_recons_psplines = mfpca_psplines.inverse_transform(scores_psplines)
+    data_recons_gram = mfpca_gram.inverse_transform(scores_gram)
+
+    ##########################################################################
+
+    ##########################################################################
+    print(f"Run MFPCA for data with noise (simulation {idx})")
+
+    # Run MFPCA with FCPTPA and UFPCA
+    univariate_expansions = [
+        {'method': 'FCPTPA', 'n_components': 20},
+        {'method': 'UFPCA', 'n_components': 15}
+    ]
+    mfpca_covariance_noise = MFPCA(
+        n_components=n_components,
+        method='covariance',
+        univariate_expansions=univariate_expansions
+    )
+    mfpca_covariance_noise.fit(data_noisy)
+
+
+    # Run MFPCA with PSplines
+    univariate_expansions = [
+        {'method': 'PSplines', 'penalty': [1, 1]},
+        {'method': 'PSplines', 'penalty': [1]}
+    ]
+    mfpca_psplines_noise = MFPCA(
+        n_components=n_components,
+        method='covariance',
+        univariate_expansions=univariate_expansions
+    )
+    mfpca_psplines_noise.fit(data_noisy)
+
+
+    # Run MFPCA with Gram matrix
+    mfpca_gram_noise = MFPCA(n_components=n_components, method='inner-product')
+    mfpca_gram_noise.fit(data_noisy)
+
+
+    # Estimate scores
+    scores_covariance_noise = mfpca_covariance_noise.transform(data_noisy)
+    scores_psplines_noise = mfpca_psplines_noise.transform(data_noisy)
+    scores_gram_noise = mfpca_gram_noise.transform(method='InnPro')
+
+
+    # Reconstructing the data
+    data_recons_covariance_noise = mfpca_covariance_noise.inverse_transform(
+        scores_covariance_noise
+    )
+    data_recons_psplines_noise = mfpca_psplines_noise.inverse_transform(
+        scores_psplines_noise
+    )
+    data_recons_gram_noise = mfpca_gram_noise.inverse_transform(
+        scores_gram_noise
+    )
+
+    ##########################################################################
+
+    results = {
+        # Time
+        'time_covariance': time_covariance,
+        'time_psplines': time_psplines,
+        'time_gram': time_gram,
+        # Data
+        'data_true': data,
+        'eigenfunctions_true': eigenfunctions,
+        'eigenvalues_true': eigenvalues,
+        # Estimation data no noise
+        'mfpca_covariance': mfpca_covariance,
+        'mfpca_psplines': mfpca_psplines,
+        'mfpca_gram': mfpca_gram,
+        'data_recons_covariance': data_recons_covariance,
+        'data_recons_psplines': data_recons_psplines,
+        'data_recons_gram': data_recons_gram,
+        # Estimation data noise
+        'mfpca_covariance_noise': mfpca_covariance_noise,
+        'mfpca_psplines_noise': mfpca_psplines_noise,
+        'mfpca_gram_noise': mfpca_gram_noise,
+        'data_recons_covariance_noise': data_recons_covariance_noise,
+        'data_recons_psplines_noise': data_recons_psplines_noise,
+        'data_recons_gram_noise': data_recons_gram_noise
     }
+
+    NAME = f'{PATH}/simulation_{idx}.pkl'
+    with open(NAME, "wb") as f:
+        pickle.dump(results, f)
 
 
 # Run
 if __name__ == "__main__":
     print("Run the simulation")
 	
-    start = time.time()
-    results = Parallel(n_jobs=NUM_CORES)(
-        delayed(run_simulation)(idx, N, M, n_components)
+    if not os.path.exists(f"{args.out_folder}"):
+        os.makedirs(f"{args.out_folder}")
+
+    PATH = args.out_folder
+
+    start = time.process_time()
+    Parallel(n_jobs=NUM_CORES)(
+        delayed(run_simulation)(idx, N, noise_variance, n_components, PATH)
         for idx in range(N_SIMU)
     )
-    print(f'{time.time() - start}')
- 
-    if not os.path.exists(f"{args.out_folder}/P{P}"):
-        os.makedirs(f"{args.out_folder}/P{P}")
-
-    NAME = f'results_{N}_{M}_{P}_{N_SIMU}.json'
-    with open(f'{args.out_folder}/P{P}/{NAME}', 'w') as outfile:
-        json.dump(results, outfile)
+    print(f'{time.process_time() - start}')
