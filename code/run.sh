580de61ea1fd892c74e689949540876266de4701
StevenGolovkine <steven_golovkine@icloud.com>
2024-05-17 20:03:59 +0100

    Simulation

--- code/run.sh
+++ code/run.sh
@@ -3,19 +3,19 @@
 for N in 50 100 250
 do
     echo "Simulation N=$N - M=11/11/21"
-    python ./code/main.py -nsimu 200 -nobs $N -npoints 11 11 21 -noise 0 -percentages 0.5 0.5  -k 12 ./results
+    python ./code/main.py -nsimu 200 -nobs $N -npoints 11 11 21 -noise 0.25 -percentages 0.5 0.5  -k 12 ./results
 done
 
 
 for N in 50 100 250
 do
     echo "Simulation N=$N - M=26/26/51"
-    python ./code/main.py -nsimu 200 -nobs $N -npoints 26 26 51 -noise 0 -percentages 0.5 0.5  -k 12 ./results
+    python ./code/main.py -nsimu 200 -nobs $N -npoints 26 26 51 -noise 0.25 -percentages 0.5 0.5  -k 12 ./results
 done
 
 
 for N in 50 100 250
 do
     echo "Simulation N=$N - M=101/51/201"
-    python ./code/main.py -nsimu 200 -nobs $N -npoints 101 51 201 -noise 0 -percentages 0.5 0.5  -k 12 ./results
+    python ./code/main.py -nsimu 200 -nobs $N -npoints 101 51 201 -noise 0.25 -percentages 0.5 0.5  -k 12 ./results
 done
